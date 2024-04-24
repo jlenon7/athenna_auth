8f81c14581c17a50e64c90d2149b43e412bddbe3
jlenon7 <lenonSec7@gmail.com>
2024-04-24 16:04:30 +0100

    feat(response): add baseUrl, reason and responseTime

--- src/interceptors/response.interceptor.ts
+++ src/interceptors/response.interceptor.ts
@@ -1,12 +1,30 @@
+import { Parser } from '@athenna/common'
 import { Interceptor } from '@athenna/http'
 import type { InterceptorContract, InterceptContext } from '@athenna/http'
 
 @Interceptor({ name: 'response' })
 export class ResponseInterceptor implements InterceptorContract {
-  public async intercept({ response }: InterceptContext): Promise<unknown> {
-    return {
+  public async intercept({
+    request,
+    response
+  }: InterceptContext): Promise<unknown> {
+    const body: any = {
       status: response.statusCode,
-      data: response.body
+      code: Parser.statusCodeToReason(response.statusCode),
+      baseUrl: request.baseUrl,
+      responseTime: response.responseTime
     }
+
+    if (response.body.meta && response.body.links) {
+      body.meta = response.body.meta
+      body.links = response.body.links
+      body.data = response.body.data
+
+      return body
+    }
+
+    body.data = response.body
+
+    return body
   }
 }
