d21eccb1cafd1348730e581a177dda72f4c2ab18
jlenon7 <lenonSec7@gmail.com>
2024-04-28 16:20:47 +0100

    ci(env): add exception logs

--- src/config/logging.ts
+++ src/config/logging.ts
@@ -19,7 +19,8 @@ export default {
     exception: {
       level: 'trace',
       formatter: 'none',
-      driver: Env('APP_ENV') === 'test' ? 'null' : 'console',
+      driver: 'console',
+      // driver: Env('APP_ENV') === 'test' ? 'null' : 'console',
       streamType: 'stderr'
     },
 
